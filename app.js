6086c1da2f7ba2349fd7adfa160906944cdf8dc6
Krzysztof Królikowski <kkrolikowski@gmail.com>
2023-05-15 18:42:26 +0000

    up

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res,next) => {
-    res.write("Hello world from gitpod!");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const mainRoutes = require('./routes/main');
+app.use(mainRoutes);
+
+app.use((req,res,next) => {
+    res.status(404).render('404');
 });
 app.listen(3000);
